0f68784a192e1b082c85d9b95eedfd8e8e1bb835
will c <will.cra@outlook.com>
2020-07-23 22:01:57 -0400

    console logged a basic parameter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-Recipes';
+
+  recipesParams: object = {
+    q: "chicken",
+    health: "peanut-free",
+    diet: "low-fat"
+  }
+
+  constructor(private service: RecipeService){}
+
+  ngOnInit(){
+    this.service.getRecipes(this.recipesParams).subscribe(data =>{
+      console.log(data);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
